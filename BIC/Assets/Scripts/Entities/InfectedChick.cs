5b54449b85074378d7115107ac0acf5eb42ec66f
MagicDylen <lpbdiego@gmail.com>
2023-07-08 23:09:40 +0200

    Infected Chicken

---
+++ BIC/Assets/Scripts/Entities/InfectedChick.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectedChick : Predator
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the entered GameObject is not the detector itself
+        if (other.gameObject != gameObject && chaseTarget == null)
+        {
+            if (other.CompareTag("Predator") || other.CompareTag("Player"))
+            {
+                // The entered GameObject is within the range
+                Debug.Log(other.gameObject.name + " entered the range of Infected Chick!");
+                chaseTarget = other.gameObject.transform;
+                StartChasing();
+            }
+
+        }
+    }
+}
