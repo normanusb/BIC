08ef92567a79cff02fd287322c59a8a37ade74d3
Astrobit <miguellpb96@hotmail.com>
2023-07-08 23:28:40 +0200

    Started implementing sound on egg. Not done yet but need to pull

--- BIC/Assets/Scripts/Entities/egg.cs
+++ BIC/Assets/Scripts/Entities/egg.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class PlayerCollectEggs : MonoBehaviour
 {
   public GameObject chickprefab;
+
+    //VFX
+    [SerializeField] private EventReference eggCollectedSound;
+
   private void OnTriggerEnter(Collider other) 
   {
     Player player = other.GetComponent<Player>();
 
     if (player != null) 
     {
+        //SFX
+        AudioManager.instance.PlayOneShot(eggCollectedSound);
         Instantiate(chickprefab, transform.position, transform.rotation);
         Destroy(this.gameObject);
     }
