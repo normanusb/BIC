56163a88aa71cfdb6f552aaf4c37778b7c02c491
LAPTOP-EUJHT6RI\Norma <normanusb@gmail.com>
2023-07-08 14:04:57 +0200

    Camera and character movement

--- BIC/Assets/Scripts/Entities/Player.cs
+++ BIC/Assets/Scripts/Entities/Player.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    public float speed;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    public void movePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
